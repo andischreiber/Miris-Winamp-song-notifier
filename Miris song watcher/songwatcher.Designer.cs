fcbe05203fe684992593e8fbf7ef2c7455c16f63
andischreiber <IE0hd9xXl8>
2014-08-15 19:03:05 +0200

    initial commit

---
+++ Miris song watcher/songwatcher.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Miris_Songsaver
+{
+    partial class songwatcher
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(songwatcher));
+            this.lbl_CurrentTitle = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.cb_voice = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_CurrentTitle
+            // 
+            this.lbl_CurrentTitle.AutoSize = true;
+            this.lbl_CurrentTitle.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_CurrentTitle.Location = new System.Drawing.Point(14, 19);
+            this.lbl_CurrentTitle.Name = "lbl_CurrentTitle";
+            this.lbl_CurrentTitle.Size = new System.Drawing.Size(156, 45);
+            this.lbl_CurrentTitle.TabIndex = 0;
+            this.lbl_CurrentTitle.Text = "Welcome";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1037, 327);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(226, 48);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // cb_voice
+            // 
+            this.cb_voice.AutoSize = true;
+            this.cb_voice.Checked = true;
+            this.cb_voice.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb_voice.Location = new System.Drawing.Point(26, 105);
+            this.cb_voice.Name = "cb_voice";
+            this.cb_voice.Size = new System.Drawing.Size(89, 19);
+            this.cb_voice.TabIndex = 4;
+            this.cb_voice.Text = "voice output";
+            this.cb_voice.UseVisualStyleBackColor = true;
+            // 
+            // songwatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1275, 387);
+            this.Controls.Add(this.cb_voice);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.lbl_CurrentTitle);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "songwatcher";
+            this.Text = "Miri`s song watcher";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_CurrentTitle;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.CheckBox cb_voice;
+    }
+}
+
---
+++ Miris song watcher/songwatcher.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using WinampFrontEndLib;
+
+namespace Miris_Songsaver
+{
+    public partial class songwatcher : Form
+    {
+        private string strWATitlePast;
+        private string strWATitle;
+        private string strWATitleTalk;
+        private int WAstatus;
+        private string strTTSURL;
+
+        public System.Windows.Forms.Timer t1 = new System.Windows.Forms.Timer();
+        WebClient wc;
+
+        public songwatcher()
+        {
+            InitializeComponent();
+            //Timer
+            t1.Interval = 1000;                                 // Interval ms
+            t1.Tick += new EventHandler(t1_Tick);               // Eventhandler timer
+            t1.Start();
+            axWindowsMediaPlayer1.settings.volume = 100;        // max. volume
+        }
+
+        void t1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                WAstatus = WinampLib.GetPlaybackStatus();       // Winamp states: 3=pause,1=play,0=stop
+                switch (WAstatus)
+                {
+                    case 1: // Winamp playing
+                        strWATitlePast = strWATitle;
+                        strWATitle = WinampLib.GetCurrentSongTitle();
+                        lbl_CurrentTitle.Text = strWATitle;
+                        if (strWATitle != strWATitlePast)       // new song started
+                        {
+                            axWindowsMediaPlayer1.URL = "none.mp3";                     // dummy input (else cannot overwrite speech.mp3 with new file)
+                            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+                            {
+                                axWindowsMediaPlayer1.Ctlcontrols.stop();
+                            }
+                            axWindowsMediaPlayer1.close();
+                            strWATitleTalk = strWATitle.Replace("&", "%26");            // correct URL chars
+                            strTTSURL = "http://translate.google.com/translate_tts?tl=en&q=" + strWATitleTalk;
+
+                            if ((strWATitle.Length < 100) && (cb_voice.Checked))        // max. length 100 chars
+                            {
+                                
+                                // download voice file
+                                using (wc = new WebClient())
+                                {
+                                    wc.DownloadFileCompleted += new AsyncCompletedEventHandler(DLCompleted);
+                                    //wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                                    Uri URL = new Uri(strTTSURL);
+
+                                    try
+                                    {
+                                        // download
+                                        wc.DownloadFileAsync(URL, "speech.mp3");
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        MessageBox.Show(ex.Message);
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    case 3: // Winamp paused
+                        lbl_CurrentTitle.Text = "P A U S E";
+                        break;
+                    default:
+                        strWATitle = "";
+                        strWATitlePast = "";
+                        strWATitleTalk = "";
+                        lbl_CurrentTitle.Text = "";
+                        break;
+                }
+            }
+            catch { }
+        }
+
+        private void DLCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            // download completed - play sound
+            if (e.Cancelled == true)
+            {
+                Console.Beep();
+            }
+            else
+            {
+                // voice output section
+                axWindowsMediaPlayer1.URL = "speech.mp3";
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+    }
+}
